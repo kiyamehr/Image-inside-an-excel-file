658a7de0f799715b46e590d1f7a464e0057d02aa
Kiamehr <kiamehrmoradii@gmail.com>
2025-11-09 10:55:44 +0330

    made it work

--- main.py
+++ main.py
@@ -1,39 +1,30 @@
 from PIL import Image
 import openpyxl
-from openpyxl.styles import fills
+from openpyxl.styles import PatternFill
 
 work_book = openpyxl.Workbook()
 work_sheet = work_book.active
 
-
-img = Image.open("images.jpg")
+img = Image.open("images.jpg").convert("RGB")
 pix = img.load()
 
 
-def cell_finder(y, x):
-    for y_cell in range(1, y):
-        for x_cell in range(1, x):
-            cell_name = work_sheet.cell(row=y_cell, column=x_cell)
-            cell_coordinate = cell_name.coordinate
-            return cell_coordinate
 # for image loop
-for y in range(0, 255, 10):
-    for x in range(0, 198, 10):
-        pixels = pix[y, x]
-        r, g, b = pixels
-        hex_color = "{:02X}{:02X}{:02X}".format(r, g, b)
-        
-        fill = fills.PatternFill(
-            start_color="ff" + hex_color, end_color="ff" + hex_color, fill_type="solid"
-        )
-        
+for y in range(0, 198, 1):
+    for x in range(0, 255, 1):
+        r, g, b = pix[x, y]
 
-        # for excel loop
-        for y_cell in range(1, y):
-            for x_cell in range(1, x):
-                cell_name = work_sheet.cell(row=y_cell, column=x_cell)
-                cell_coordinate = cell_name.coordinate
-                
-        work_sheet[f"{cell_coordinate}"].fill = fill
+        hex_color = "{:02X}{:02X}{:02X}".format(r, g, b)
+        fill = PatternFill(
+            start_color="FF" + hex_color,
+            end_color="FF" + hex_color,
+            fill_type="solid"
+        )
+
+        # put directly instead of finding separately
+        work_sheet.cell(row=y // 10 + 1, column=x // 10 + 1).fill = fill
+
+
+
 
 work_book.save("image_excel.xlsx")
